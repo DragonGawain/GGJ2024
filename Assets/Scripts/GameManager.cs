7f3e69a543bccdaf76abef92670fb636b7f8c47a
Craig <craigk00@hotmail.com>
2024-01-27 08:26:44 -0500

    GamaManager init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int passiveTimer = 20 * 50;
+    int attackTimer = 10 * 50;
+    bool attackTime = false;
+    int timer = 0;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer++;
+        switch (attackTime)
+        {
+            // attack wave
+            case true:
+                if (timer >= attackTimer)
+                {
+                    timer = 0;
+                    attackTime = false;
+                }
+                break;
+            // passive wave
+            case false:
+                if (timer >= passiveTimer)
+                {
+                    timer = 0;
+                    attackTime = true;
+                }
+                break;
+        }
+    }
+}
