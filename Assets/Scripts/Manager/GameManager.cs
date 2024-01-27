76ff6a10cb27a1d1e429a84eb7407d0460c77c4d
Craig <craigk00@hotmail.com>
2024-01-27 13:31:19 -0500

    vector2 rotate util

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -187,4 +187,13 @@ public class GameManager : MonoBehaviour
 
         uiManager.EndGame();
     }
+
+    public static Vector2 rotate(Vector2 v, float delta)
+    {
+        delta *= Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
 }
