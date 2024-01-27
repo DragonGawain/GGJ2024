1f6cb7202192f9256f52a311af58c417a1f04a40
Craig <craigk00@hotmail.com>
2024-01-27 10:09:56 -0500

    Cannons init

--- Assets/Scripts/Cannons/Cannon.cs
+++ Assets/Scripts/Cannons/Cannon.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cannon : MonoBehaviour
+public abstract class Cannon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected cheese cannonType;
+    protected int ammo;
+    protected int maxAmmo;
+
+    public void UpgradeAmmo(int qt)
     {
-        
+        maxAmmo += qt;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int getAmmo()
     {
-        
+        return ammo;
+    }
+
+    public int setAmmo(int qt)
+    {
+        int leftover = 0;
+        ammo += qt;
+        if (ammo > maxAmmo)
+        {
+            leftover = ammo - maxAmmo;
+            ammo = maxAmmo;
+        }
+        return leftover;
     }
 }
--- Assets/Scripts/Cannons/Melted.cs
+++ Assets/Scripts/Cannons/Melted.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Melted : Cannon
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cannonType = cheese.MELTED;
     }
 
     // Update is called once per frame
