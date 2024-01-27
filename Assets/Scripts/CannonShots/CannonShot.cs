85c71030df660c8fe9c7a1de8a0f6a638354122e
Craig <craigk00@hotmail.com>
2024-01-27 12:25:53 -0500

    shot layer

--- Assets/Scripts/CannonShots/CannonShot.cs
+++ Assets/Scripts/CannonShots/CannonShot.cs
@@ -10,6 +10,15 @@ public abstract class CannonShot : MonoBehaviour
     public void StartMove(Vector2 direction)
     {
         body.velocity = direction;
+        switch (shellType)
+        {
+            case cheese.MELTED:
+                break;
+            case cheese.SHREDDED:
+                break;
+            case cheese.CURD:
+                break;
+        }
         if (shellType == cheese.SHREDDED || shellType == cheese.CURD)
         {
             body.velocity *= 3;
