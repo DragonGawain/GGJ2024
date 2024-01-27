076d3454561fa116c39af7b0e9e7c3e2e46640c6
Craig <craigk00@hotmail.com>
2024-01-27 17:25:49 -0500

    melted cheese update

--- Assets/Scripts/CannonShots/MeltedShot.cs
+++ Assets/Scripts/CannonShots/MeltedShot.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class MeltedShot : CannonShot
 {
+    [SerializeField]
+    float expansionRate = 0.07f;
+
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         shellType = cheese.MELTED;
         damage = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localScale = new Vector3(
+            transform.localScale.x + expansionRate,
+            transform.localScale.y + expansionRate,
+            +transform.localScale.z
+        );
     }
 }
