e80679e89fda7869b8be08ce7aa661792f70c37b
Craig <craigk00@hotmail.com>
2024-01-27 08:41:10 -0500

    Deposits init

--- Assets/Scripts/ResourceDeposit.cs
+++ Assets/Scripts/ResourceDeposit.cs
@@ -7,10 +7,14 @@ public class ResourceDeposit : MonoBehaviour
     [SerializeField, Range(0, 20)] int maxQuantity = 5;
     int currentQuantity;
     bool active = true;
+    cheese type;
+    // number of attack waves that need to pass for this resource deposit to replenish
+    [SerializeField, Range(0, 20)] int replensihTimer = 2;
     // Start is called before the first frame update
     void Awake()
     {
         currentQuantity = maxQuantity;
+        Replenish();
     }
 
     // Update is called once per frame
@@ -18,7 +22,31 @@ public class ResourceDeposit : MonoBehaviour
     {
         
     }
-
-    
+    public void ReplenishAttempt()
+    {
+        replensihTimer--;
+        if (replensihTimer == 0)
+        {
+            Replenish();
+            replensihTimer = 2;
+        }
+    }
+    void Replenish()
+    {
+        int choice = Mathf.FloorToInt(Random.Range(0,2.99f));
+        switch (choice)
+        {
+            case 0:
+                type = cheese.MELTED;
+                break;
+            case 1:
+                type = cheese.SHREDDED;
+                break;
+            case 2:
+                type = cheese.CURD;
+                break;
+        }
+        currentQuantity = maxQuantity;
+    }
 
 }
