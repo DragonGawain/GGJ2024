f378330003ceda04d6215528b0eee2886203d699
Zaid Minhas <61346884+ZaidMinhas@users.noreply.github.com>
2024-01-27 21:28:44 -0500

    Wave counter

--- Assets/IAmGoingToExplode.cs
+++ Assets/IAmGoingToExplode.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class IAmGoingToExplode : MonoBehaviour
 {
     public GameObject END;
+    public GameObject GameScreen;
     public void ENDME()
     {
+        GameScreen.SetActive(false);
         print("YES");
         END.SetActive(true);
+    }
+
+    
+    public GameObject waveCountText;
+    public void Wave(int waveNum)
+    {
+        waveCountText.GetComponent<TextMeshProUGUI>().text = "Wave: " + waveNum;
     }
 }
